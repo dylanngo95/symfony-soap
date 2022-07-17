18e25d1100e72ce12e4e10e850dd3ac96aefe600
Dylan Ngo <it.tinhngo@gmail.com>
2022-07-17 22:55:02 +0800

    suport get list

--- src/Controller/Service/UserService.php
+++ src/Controller/Service/UserService.php
@@ -2,15 +2,29 @@
 
 namespace App\Controller\Service;
 
+use stdClass;
+
 class UserService
 {
     /**
      * @param $email
-     * @param $password
-     * @return string
+     * @return stdClass
      */
-    public function user($email, $password): string
+    public function user($email)
     {
-        return "Your email is " . $email . '-' . $password;
+        $user1 = new StdClass();
+        $user1->email = "email1@gmail.com";
+        $user1->password = "1234";
+
+        $user2 = new StdClass();
+        $user2->email = "email2@gmail.com";
+        $user2->password = "1234";
+
+        $userArray[] = $user1;
+        $userArray[] = $user2;
+
+        $users = new StdClass();
+        $users->User = $userArray;
+        return $users;
     }
 }
